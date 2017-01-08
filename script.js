d3fdc51380893a7bd16c988326f444e9f1af8c8f
Susana Isaza <susisaza@gmail.com>
2017-01-08 19:43:10 +0100

    offset effect

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ console.log('js connected');
 $(function() {
   console.log("Dom ok");
 
-  // animate bubbles initial show:
-  // function displayBubbles() {
-  //   setTimeout(
-  //     ($('#bubbleAboutMe').css({'visibility': 'visible'})), 1000)
-  // };
-  // displayBubbles();
+  // page position bubble effects
+  $(window).scroll(function(){
+    var positionNow = $(document).scrollTop();
+    // console.log(positionNow);
+    var limiting = 400;
+    if(positionNow <= limiting){
+      // console.log("change more!!!");
+      $('.bubble').animate({
+        "width":"200px",
+        "height": "200px",
+        "fontSize": "30px",
+        "lineHeight": "120px"
+      }, 10);
+    } if(positionNow > limiting) {
+      // console.log("change less!!!");
+      $('.bubble').animate({
+        "width":"150px",
+        "height": "150px",
+        "fontSize": "20px",
+        "lineHeight": "100px"
+      }, 15);
+    }
+  });
 
   // action window scroll to next veiew on click on bubble
   $('#bubbleAboutMe').on('click', (event) => {
@@ -16,37 +33,37 @@ $(function() {
       scrollTop: $('#bubbleAboutMe').offset().top -10
     }, 500);
     $('#modalAboutMe').css({'visibility': 'visible'});
-    $('.bubble').css({
-      width: '150px',
-      height: '150px',
-      fontSize: '20px',
-      lineHeight: '100px',
-    });
+    // $('.bubble').css({
+    //   width: '150px',
+    //   height: '150px',
+    //   fontSize: '20px',
+    //   lineHeight: '100px',
+    // });
   });
   $('#bubbleProjects').on('click', (event) => {
     $('body').animate({
       scrollTop: $('#bubbleProjects').offset().top -10
     }, 500);
     $('#modalProjects').css({'visibility': 'visible'});
-    $('.bubble').css({
-      width: '150px',
-      height: '150px',
-      fontSize: '20px',
-      lineHeight: '100px',
-    });
+    // $('.bubble').css({
+    //   width: '150px',
+    //   height: '150px',
+    //   fontSize: '20px',
+    //   lineHeight: '100px',
+    // });
   });
   $('#bubbleContact').on('click', (event) => {
     $('body').animate({
       scrollTop: $('#bubbleContact').offset().top -10
     }, 500);
     $('#modalContact').css({'visibility': 'visible'});
-    $('.bubble').css({
-      width: '150px',
-      height: '150px',
-      fontSize: '20px',
-      lineHeight: '100px',
-      marginBottom: '10px'
-    });
+    // $('.bubble').css({
+    //   width: '150px',
+    //   height: '150px',
+    //   fontSize: '20px',
+    //   lineHeight: '100px',
+    //   marginBottom: '10px'
+    // });
   });
   // Modal response to hovering over different bubbles
   $('#bubbleProjects').on('mouseover', (function(event) {
